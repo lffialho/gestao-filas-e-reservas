5bc2ea9929c034d3288f5c2b75b3b2ef1512f111
Luiz Fialho <lfialho.dev@gmail.com>
2026-08-31 13:00:37 -0300

    feat(dominio): orquestracao de reservas com MotorGerente

---
+++ src/dominio/servicos/motor-gerente.js
@@ -0,0 +1,78 @@
+import { TravaAssincrona } from "../../compartilhado/assincrono/trava-assincrona.js";
+import { Mesa } from "../entidades/mesa.js";
+import { FilaDeEspera } from "../entidades/fila-de-espera.js";
+
+export class MotorGerente {
+    #mesas;
+    #trava;
+    #filaDeEspera
+
+    constructor(){
+        this.#mesas = new Map()
+        this.#trava = new TravaAssincrona()
+        this.#filaDeEspera = new FilaDeEspera()
+    }
+
+    adicionarMesa(mesa){
+        return this.#mesas.set(mesa.id, mesa)
+    }
+
+    buscarMesa(id){
+        return this.#mesas.get(id)
+    }
+
+    entrarNaFila(cliente, tamanhoGrupo, telefone){
+        return this.#filaDeEspera.adicionar(cliente,tamanhoGrupo,telefone)
+    }
+
+    get tamanhoFilaEspera(){
+        return this.#filaDeEspera.tamanhoDaFila
+    }
+
+    async fazerReserva(mesaId, cliente, tamanhoGrupo){
+        const mesa = this.buscarMesa(mesaId)
+        if (!mesa){
+            throw new Error("Mesa não encontrada.");
+        }
+        return await this.#trava.executarComExclusividade(mesaId, async() =>  {
+            mesa.reservar(tamanhoGrupo)
+            return {
+                sucesso: true,
+                cliente,
+                mesaNumero: mesa.numero,
+                status: mesa.status
+            }
+        })
+    }
+
+    async liberaMesa(mesaId){
+        return await this.#trava.executarComExclusividade(mesaId, async() => {
+            const mesa = this.buscarMesa(mesaId)
+
+        if (!mesa){
+            throw new Error("Mesa não encontrada.");
+        }
+
+        mesa.liberarMesa()
+
+        const proximoCliente = this.#filaDeEspera.proximoCompativel(mesa.capacidade)
+            
+        if (proximoCliente !== null){
+                mesa.reservar(proximoCliente.tamanhoGrupo)
+            return {
+                sucesso: true,
+                mesaNumero: mesa.numero,
+                atendido: proximoCliente.cliente,
+                status: mesa.status
+            }
+        }else{
+            return{
+                sucesso: true,
+                mesaNumero: mesa.numero,
+                mensagem: "Mesa Liberada e sem clientes na fila!"
+            }
+        }})
+    }  
+}
+
+
