c5315a56b1e05f8bf311ddf5ef2742ccbbdf9f1d
Luiz Fialho <lfialho.dev@gmail.com>
2026-09-14 16:37:09 -0300

    fix(ferramentas): BOM nos .ps1, senao o PowerShell 5.1 corrompe os acentos

--- ferramentas/desinstalar-windows.ps1
+++ ferramentas/desinstalar-windows.ps1
@@ -1,4 +1,4 @@
-#Requires -Version 5.1
+﻿#Requires -Version 5.1
 <#
 .SYNOPSIS
     Tira o salão do arranque do Windows.
--- ferramentas/instalar-windows.ps1
+++ ferramentas/instalar-windows.ps1
@@ -1,4 +1,4 @@
-#Requires -Version 5.1
+﻿#Requires -Version 5.1
 <#
 .SYNOPSIS
     Deixa o salão subindo sozinho na máquina do balcão.
